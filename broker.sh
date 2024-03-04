e5507b3e3e9c3fe2c54fd5026159d3fcf99968a1
Iurii Kogan <koganiurii@gmail.com>
2024-03-04 12:12:23 +0200

    broker deploy

--- broker.sh
+++ broker.sh
@@ -8,26 +8,28 @@
 #
 # set the required environment variables
 export BROKER_TOKEN=$BROKER_TOKEN
-export GITLAB_URL=$GITLAB_URL
-export GITLAB_TOKEN=$GITLAB_TOKEN
-export BROKER_CLIENT_URL=$BROKER_CLIENT_URL
-export BROKER_CLIENT_PORT=$BROKER_CLIENT_PORT
+export GITHUB_URL=$GITHUB_URL
+export GITHUB_TOKEN=$GITHUB_TOKEN
+export BROKER_CLIENT_URL=http://192.168.100.6.nip.io
+export BROKER_CLIENT_PORT=8000
 
-if [ "$1" == "destroy" ]; then
-    helm uninstall snyk-broker-chart -n snyk-broker
-    kubectl delete namespace snyk-broker
-    exit 0
-fi
-
-if [ "$1" == "deploy" ]; then
-    helm repo add snyk-broker https://snyk.github.io/snyk-broker-helm/ --force-update
+    helm repo add snyk-broker https://snyk.github.io/snyk-broker-helm/
+    helm repo update
     helm install snyk-broker-chart snyk-broker/snyk-broker \
-             --set scmType=gitlab \
+             --set scmType=github \
              --set brokerToken=${BROKER_TOKEN} \
-             --set gitlab=${GITLAB_URL} \
+             --set gitlab=${GITHUB_URL} \
              --set scmToken=${GITLAB_TOKEN} \
              --set brokerClientUrl=${BROKER_CLIENT_URL}:${BROKER_CLIENT_PORT} \
              -n snyk-broker --create-namespace
-    exit 0
-fi
 
+# Run in Docker
+    docker run --restart=always -d -p 8000:8000 \
+        -e BROKER_TOKEN=${BROKER_TOKEN} \
+        -e GITHUB_TOKEN=${GITHUB_TOKEN} \
+        -e PORT=8000 \
+        -e BROKER_CLIENT_URL=http://192.168.100.6.nip.io:8000 \
+        -e ACCEPT_IAC=tf,yaml,yml,json,tpl \
+        -e ACCEPT_CODE=true \
+        -e ACCEPT_APPRISK=true \
+        snyk/broker:github-com
