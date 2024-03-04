785451bf61c89898afdea0a9c380adec6a011cb0
Iurii Kogan <koganiurii@gmail.com>
2024-03-04 16:01:48 +0200

    update broker.sh

--- broker.sh
+++ broker.sh
@@ -8,18 +8,19 @@
 #
 # curl the broker token from the Snyk API
 # 
-curl --include \
-    --request POST \
-     --header "Content-Type: application/json" \
-     --header "Authorization: token $SNYK_TOKEN" \
-  'https://api.snyk.io/v1/org/'${SNYK_ORG_ID}'/integrations/'${SNYK_INTEGRATION_ID}'/authentication/provision-token'
+# curl --include \
+#     --request POST \
+#     --header "Content-Type: application/json" \
+#     --header "Authorization: ${SNYK_TOKEN}" \
+#   'https://api.snyk.io/v1/org/${SNYK_ORG}'/integrations/'${INTEGRATION_ID}'/authentication/provision-token'
+
 
 # set the required environment variables before running this script
 export BROKER_TOKEN=$BROKER_TOKEN
 export GITHUB_URL=$GITHUB_URL
 export GITHUB_TOKEN=$GITHUB_TOKEN
 export BROKER_CLIENT_URL=http://192.168.100.6.nip.io
-export BROKER_CLIENT_PORT=8000
+export BROKER_CLIENT_PORT=8080
 
     helm repo add snyk-broker https://snyk.github.io/snyk-broker-helm/
     helm repo update
@@ -32,12 +33,11 @@ export BROKER_CLIENT_PORT=8000
              -n snyk-broker --create-namespace
 
 # Run in Docker
-    docker run --restart=always -d -p 8000:8000 \
+    docker run --restart=always -d -p 8080:8080 \
         -e BROKER_TOKEN=${BROKER_TOKEN} \
         -e GITHUB_TOKEN=${GITHUB_TOKEN} \
         -e PORT=8000 \
-        -e BROKER_CLIENT_URL=http://192.168.100.6.nip.io:8000 \
+        -e BROKER_CLIENT_URL=http://broker.192.168.100.6.nip.io \
         -e ACCEPT_IAC=tf,yaml,yml,json,tpl \
         -e ACCEPT_CODE=true \
-        -e ACCEPT_APPRISK=true \
         snyk/broker:github-com
